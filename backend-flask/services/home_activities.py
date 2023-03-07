9d59fdb730d823aa18c8d9f5bbd1afc2f7e2067f
Marv <murithimarvin09@gmail.com>
2023-03-07 14:19:44 +0000

    Cognito intergration

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -4,7 +4,7 @@ from opentelemetry import trace
 tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
-  def run(logger):
+  def run():
     #logger.info("home activities")
     with tracer.start_as_current_span("home-activites-mock-data"):
       span = trace.get_current_span()
