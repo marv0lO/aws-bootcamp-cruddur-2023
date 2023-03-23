ad2fb4356e64d712c3bf866286ec6f32a14eafbf
Marv <murithimarvin09@gmail.com>
2023-03-23 08:25:35 +0000

    fix

--- aws/json/lambdas/cruddur-post-confirmation.py
+++ aws/json/lambdas/cruddur-post-confirmation.py
@@ -1,48 +1,45 @@
-import os
 import json
 import psycopg2
-
+import os
 
 def lambda_handler(event, context):
     user = event['request']['userAttributes']
-    user_display_name = user["name"]
-    user_email = user["email"]
-    user_handle = user["preferred_username"]
-    user_cognito_id = user["sub"]
+    print('userAttributes')
+    print(user)
 
+    user_display_name  = user['name']
+    user_email         = user['email']
+    user_handle        = user['preferred_username']
+    user_cognito_id    = user['sub']
     try:
-        conn = psycopg2.connect(os.getenv("CONNECTION_URL"))
-        cur = conn.cursor()
-        sql = f"""
-        INSERT INTO public.users (
-            display_name,
-            email, 
-            handle, 
-            cognito_user_id
-            ) 
-        VALUES(
-            %s, %s, %s, %s
-            )
-        """
-        params = [
-            user_display_name,
-            user_email,
-            user_handle,
-            user_cognito_id
-        ]
-
-        print(sql)
-        cur.execute(sql, params)
-        conn.commit()
-        print('Commit Done')
+      print('entered-try')
+      sql = f"""
+         INSERT INTO public.users (
+          display_name, 
+          email,
+          handle, 
+          cognito_user_id
+          ) 
+        VALUES(%s,%s,%s,%s)
+      """
+      print('SQL Statement ----')
+      print(sql)
+      conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
+      cur = conn.cursor()
+      params = [
+        user_display_name,
+        user_email,
+        user_handle,
+        user_cognito_id
+      ]
+      cur.execute(sql,params)
+      conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-
+      print(error)
     finally:
-        if conn is not None:
-            cur.close()
-            conn.close()
-            print('Database connection closed.')
-
+      if conn is not None:
+          cur.close()
+          conn.close()
+          print('Database connection closed.')
     return event
