fc9748d63094089565941053e4a984aed4cc5ea0
Marv <murithimarvin09@gmail.com>
2023-03-31 19:00:52 +0000

    fix: message groups

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,9 +1,9 @@
 -- this file was manually created
-INSERT INTO public.users (display_name, handle, email, cognito_user_id)
+INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown','andrewbrown', 'andrewbrown@test.com' ,'MOCK'),
-  ('Andrew Bayko', 'bayko', 'bayko@test.com' ,'MOCK'),
-  ('Marvin Murithi', 'Marv', 'marv@test.com' ,'MOCK');
+  ('Andrew Brown', 'andrewbrown@test.com', 'andrewbrown','MOCK'),
+  ('Andrew Bayko', 'bayko@test.com', 'bayko' ,'MOCK'),
+  ('Marvin Murithi', 'marv@test.com', 'Marv' ,'MOCK');
 
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
