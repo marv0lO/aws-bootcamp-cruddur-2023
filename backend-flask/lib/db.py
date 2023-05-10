09af4b0771e322218c2384fd9a9cf99ebcd4b56e
Marv <murithimarvin09@gmail.com>
2023-05-10 15:24:43 +0000

    implement migrations, profile page and endpoint to update profile

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -42,8 +42,9 @@ class Db:
 
     # we want to commit data such as an insert
     # be sure to check for RETURNING in all uppercases
-    def query_commit(self, sql, params={}):
-        self.print_sql('commit with returning', sql, params)
+    def query_commit(self, sql, params={},verbose=True):
+        if verbose:
+            self.print_sql('commit with returning', sql, params)
 
         pattern = r"\bRETURNING\b"
         is_returning_id = re.search(pattern, sql)
@@ -61,8 +62,9 @@ class Db:
             self.print_sql_err(err)
 
     # when we want to return a json object
-    def query_array_json(self, sql, params={}):
-        self.print_sql('array', sql, params)
+    def query_array_json(self, sql, params={},verbose=True):
+        if verbose:
+            self.print_sql('array', sql, params)
 
         wrapped_sql = self.query_wrap_array(sql)
         with self.pool.connection() as conn:
@@ -72,10 +74,10 @@ class Db:
                 return json[0]
 
     # when we want to return an array of json objects
-    def query_object_json(self, sql, params={}):
-
-        self.print_sql('json', sql, params)
-        self.print_params(params)
+    def query_object_json(self, sql, params={},verbose=True):
+        if verbose:
+            self.print_sql('json', sql, params)
+            self.print_params(params)
         wrapped_sql = self.query_wrap_object(sql)
 
         with self.pool.connection() as conn:
@@ -87,8 +89,9 @@ class Db:
                 else:
                     return json[0]
 
-    def query_value(self, sql, params={}):
-        self.print_sql('value', sql, params)
+    def query_value(self, sql, params={},verbose=True):
+        if verbose:
+            self.print_sql('value', sql, params)
         with self.pool.connection() as conn:
             with conn.cursor() as cur:
                 cur.execute(sql, params)
